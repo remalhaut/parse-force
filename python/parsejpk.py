8018437720ffb040218a70456e3583c61d88af6e
Ruben DR Ramalho <rubendrr@gmail.com>
2015-11-17 10:13:32 +0200

    changed parsejpk.py

--- python/parsejpk.py
+++ python/parsejpk.py
@@ -1,22 +1,35 @@
 #!/usr/bin/python
+def mismatchstack(lscols):
+	"""
+	hstack for columns with different lengths. Columns are aligned at the top
+	"""
+	from numpy import concatenate, ones, nan, hstack
+	clength = max([len(col) for col in lscols])
+	longcols = [concatenate(c, NaN * ones((clength-len(c), 1))) for c in lscols]
+	tableout =
+
+
+
 def parsejpkfile(fname, col_heads = {'vDeflection':'d', 'smoothedCapacitiveSensorHeight':'z'}, constants = {'springConstant:':'kcant', 'sensitivity:':'sens'}):
 	from numpy import array
-	fstruc = {value:[] for value in col_heads.itervalues() + constants.itervalues()}
+	from os.path import basename
+	fstruc = {value:[] for value in col_heads.values() + constants.values()}
+	fstruc['filename'] = basename(fname)
 	with open(fname) as f:
 		rows = (row.strip().split(' ') for row in f if row.strip('#').strip()) # this generator object basically filters out empty lines and makes the others easier to process
 		for r in rows:
 			if r[0] == '#' and r[1] == 'columns:':
 				cols = {col_heads[hdr]:r.index(hdr)-2 for hdr in col_heads.iterkeys()}
-				for hdr in col_heads:
+				for hdr in col_heads.itervalues():
 					if not hdr in cols.keys():
-						print('Missing', hdr, ' column')
+						print('Missing' + hdr + ' column')
 						return
-			elif r[0] == '#' and any(r[1] == key for key in revconst.keys()):
+			elif r[0] == '#' and any(r[1] == key for key in constants.keys()):
 				fstruc[constants[r[1]]] = r[2]
 			elif r[0] != '#':
 				for hdr in cols.iterkeys():
 					fstruc[hdr].append(float(r[cols[hdr]]))
-	for hdr in col_heads.itervalues:
+	for hdr in col_heads.itervalues():
 		fstruc[hdr] = array(fstruc[hdr])
 		fstruc[hdr] = fstruc[hdr].reshape((-1,1))
 	return fstruc
@@ -40,12 +53,21 @@ def parsejpkdir(folder = ''):
 	from numpy import hstack
 	if not folder: folder = selectdir()
 	f_ls = sorted([join(folder, f) for f in listdir(folder) if isfile(join(folder, f)) and f.endswith('.txt')])
-	dirstruct = {'directory':folder}
+	dirstruct = {'directory':folder, 'kcant':'', 'sens':'', 'curves':dict()}
+	fcount = 0
 	for f in f_ls:
-		dirstruct[f] = parsejpkfile(f)
+		fcount +=1
+		dirstruct['curves'][fcount] = parsejpkfile(f)
+		for cntlprop in ['kcant','sens']:
+			if not dirstruct[cntlprop]:
+				dirstruct[cntlprop] = dirstruct['curves'][fcount][cntlprop]
+			elif dirstruct['curves'][fcount][cntlprop] != dirstruct[cntlprop]:
+				print('Cantilever characteristic ' + cntlprop + ' changes between files in folder ' + folder)
+				return
+
 	return dirstruct
 
-def jpkmat(folder = '', arg = 'n'):
+def jpkcsv(folder = '', arg = 'n'):
 	from os import listdir
 	from os.path import isdir, join, abspath, pardir, basename
 	from scipy.io import savemat
